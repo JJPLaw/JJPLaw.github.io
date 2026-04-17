ce39ef33db74e840a55935f163f632c7922711df
JJPLaw <jackpeterlaw@gmail.com>
2026-04-17 18:07:02 +0100

    implemented responsive elements and interactivity

--- content/projects/phd-figures/script.js
+++ content/projects/phd-figures/script.js
@@ -3,6 +3,8 @@
 // https://stackoverflow.com/questions/72684103/how-to-close-an-element-by-clicking-outside-of-it
 // https://kittygiraudel.com/2021/03/18/close-on-outside-click/
 
+const windowSmall = window.matchMedia("(max-width: 950px)").matches;
+
 // waits for everything to load before adding listeners to the cards
 // probably not necessary but could be generally useful
 window.addEventListener("DOMContentLoaded", () => {
@@ -14,41 +16,58 @@ window.addEventListener("DOMContentLoaded", () => {
         card.addEventListener('click', (e) => {
             let ids = cardMatcher(card);
 
-            document.querySelector('.active-card') !== null ? document.querySelector('.active-card').classList.remove('active-card') : null;
-            document.querySelector('.active-expanded-card') !== null ? document.querySelector('.active-expanded-card').classList.remove('active-expanded-card') : null;
-
-            cardToggler(ids);
-        });
-    });
-
-    closeButtons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            let ids = cardMatcher(button.parentNode);
-            cardToggler(ids);
+            if (card.classList.contains('active-card')) {
+                cardToggler(ids);
+            } else {
+                cardCloser();
+                cardToggler(ids);
+            }
         });
     });
 
     // this might not be the best way of doing it in the long run because it loops over the cards every click, but it works
-    // document.addEventListener('click', (e) => {     
+    // document.addEventListener('click', (e) => {
     //     expandedCards.forEach(
     //         (expandedCard) => {
-    //             if (!expandedCard.contains(e.target)) {
-    //                 expandedCard.classList.remove("active-expanded-card");
+    //             if (expandedCard.classList.contains('active-expanded-card')) {
+    //                 // cardCloser();
+    //                 console.log('true');
     //             }
     //         });
     // });
+
+    // closeButtons.forEach((button) => {
+    //     button.addEventListener('click', (e) => {
+    //         let ids = cardMatcher(button);
+    //         cardToggler(ids);
+    //     });
+    // });
+
+    if (windowSmall) {
+        expandedCards.forEach((expandedCard) => {
+            let ids = cardMatcher(expandedCard);
+
+            let card = document.querySelector(`#${ids.cardId}`);
+
+            card.after(expandedCard);
+        });
+
+
+    }
 });
 
 function cardMatcher(element) {
     let id = element.id;
-    let cardId, expandedCardId, ids;
+    let cardId, expandedCardId, closeCardId, ids;
     if (id.includes('card')) {
         let idNumber = id.slice(-1);
         cardId = 'card' + idNumber;
         expandedCardId = 'expanded-card' + idNumber;
+        closeCardId = 'close-card' + idNumber;
         ids = {
             "cardId": cardId,
-            "expandedCardId": expandedCardId
+            "expandedCardId": expandedCardId,
+            "closeCardId": closeCardId
         }
     }
     return ids;
@@ -57,4 +76,9 @@ function cardMatcher(element) {
 function cardToggler(ids) {
     document.querySelector(`#${ids.cardId}`).classList.toggle('active-card');
     document.querySelector(`#${ids.expandedCardId}`).classList.toggle('active-expanded-card');
+}
+
+function cardCloser() {
+    document.querySelector('.active-card') !== null ? document.querySelector('.active-card').classList.remove('active-card') : null;
+    document.querySelector('.active-expanded-card') !== null ? document.querySelector('.active-expanded-card').classList.remove('active-expanded-card') : null;
 }
